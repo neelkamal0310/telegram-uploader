a9b164df3e24da0d77b398bd921ca062afe15836
Neel <nk10399@gmail.com>
2022-09-14 00:30:59 +0530

    Add script to generate session file

---
+++ generate_session.py
@@ -0,0 +1,21 @@
+import asyncio
+import os
+import uuid
+from turtle import clear
+
+from telethon import TelegramClient
+
+from creds import api_hash, api_id
+
+
+async def main():
+    cwd = os.getcwd()
+    uid = str(uuid.uuid4())
+    client = TelegramClient(uid, api_id, api_hash)
+    phone_number = input("Phone number: ")
+    await client.start(phone_number)
+    path = f"{cwd}/{uid}.session"
+    print(f"\n\nPath to you session file is:\n{path}\n\n")
+    print("Please add this path to the session_path variable in creds.py!!")
+
+asyncio.run(main())
